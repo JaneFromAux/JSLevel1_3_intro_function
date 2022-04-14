feaf990d69b569bb6e69c417361d9bf977c35251
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-14 14:43:40 +0200

    fertig

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+
+function intro3(name, stadt, alter) {
+    console.log('Hallo, mein Name ist ' + name + '. Ich bin ' + alter + ' Jahre alt und wohne in ' + stadt);
+}
+
+intro3('Jane', 'Augsburg', '30');
